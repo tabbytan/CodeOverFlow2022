b763f4ccc1c4caffcb66680306cd6fc6d951c583
tabbytan <weiheng203@gmail.com>
2022-08-31 14:42:52 +0800

    Fixed stuff

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,19 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+function clickOn(){
+    formvalue = document.getElementById("myText").value;
+    if (formvalue >= carbonfootprintlimit){
+        dothis()
+    }
+    else{
+        dothat()
+    }
+}
+function dothis(){
+
+}
+function dothat(){
+
+}
