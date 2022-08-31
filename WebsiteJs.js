c1fecf42d82513776fd6129c88504e9275e9357b
tabbytan <weiheng203@gmail.com>
2022-08-31 14:41:12 +0800

    Javascript

---
+++ WebsiteJs.js
@@ -0,0 +1,15 @@
+function clickOn(){
+    formvalue = document.getElementById("myText").value;
+    if (formvalue >= carbonfootprintlimit){
+        dothis()
+    }
+    else{
+        dothat()
+    }
+}
+function dothis(){
+
+}
+function dothat(){
+
+}
