66c44c73dc3716e6c8c5cd59b419c0b2b47f143d
tabbytan <weiheng203@gmail.com>
2022-09-01 22:54:21 +0800

    fixed formatting (no more XXX)

--- js/scripts.js
+++ js/scripts.js
@@ -107,7 +107,7 @@ function displayResult(flag) {
         document.getElementById("gas-img").style.filter = redFilter;
     }
     else {
-        document.getElementById("gas").innerText = "Your gas consumption is XXX"
+        document.getElementById("gas").innerText = `Your gas comsumption is ${gasemissions} tonnes CO2 equivalent`
         document.getElementById("gas-img").style.filter = greenFilter;
     }
     // fuel
@@ -116,7 +116,7 @@ function displayResult(flag) {
         document.getElementById("fuel-img").style.filter = redFilter;
     }
     else {
-        document.getElementById("fuel").innerText = "Your gas consumption is XXX"
+        document.getElementById("fuel").innerText = `Your gas comsumption is ${fuelemmissions} tonnes CO2 equivalent`
         document.getElementById("fuel-img").style.filter = greenFilter;
     }
     // aviation
@@ -125,9 +125,11 @@ function displayResult(flag) {
         document.getElementById("plane-img").style.filter = redFilter;
     }
     else {
-        document.getElementById("plane").innerText = "Your gas consumption is XXX"
+        document.getElementById("plane").innerText = `Your gas comsumption is ${aviationemissions} tonnes CO2 equivalent`
         document.getElementById("plane-img").style.filter = greenFilter;
     }
+    let total = gasemissions + fuelemmissions + aviationemissions
+    document.getElementById("total").innerText = `My total carbon footprint: ${total} tonnes CO2 equivalent`;
 }
 
 
